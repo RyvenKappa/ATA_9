bc9f4ef2046d4575f6703359fbb1bdbcd73af6ee
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-04-19 00:32:25 +0200

    Detección añadida, hay problemas con D*Lite cuando se le ponen cosas en medio del camino. Si esto se arregla, se podrian definir unos goals especificos a los que ir como si fueran basura directamente

--- D_star_lite/src/waffle_lab/scripts/ROS_integration.py
+++ D_star_lite/src/waffle_lab/scripts/ROS_integration.py
@@ -9,20 +9,21 @@ import numpy as np
 import math
 import time
 import matplotlib.pyplot as plt
-########################################################################################################################
-x = 0.0
-y = 0.0
-theta = 0.0
-# Helper functions
-def findNeighbours(i, j):  # i is y and j is x
-    neighbours = []
-    for k in range(-1, 2):
-        for l in range(-1, 2):
-            if((i+k) < 505 and (l+j) < 555 and (i+k) >= -505 and (l+j) >= -555 and (k != 0 or l != 0)):
-                neighbours.append((l+j, i+k))
-    return neighbours
+from sensor_msgs.msg import PointCloud2
 
-def callback_laser(data):
+########################################################################################################################
+x = 0.0 #Posición del robot
+y = 0.0
+x_pixel_r = 0.0
+y_pixel_r = 0.0
+theta = 0.0
+
+#Angulo de distancia
+angulo_x=0.0
+angulo_y=0.0
+
+
+def callback_laser_original(data):
     global yaw
     global currentx
     global currenty
@@ -33,8 +34,16 @@ def callback_laser(data):
 
     obst_neighbours = []
     # range_min = data.range_min
+    angle_min = data.angle_min
+    angle_max = data.angle_max
+    angle_increment = data.angle_increment
     ranges = np.array(data.ranges)
-    if (min(ranges) <= scale):
+    print(ranges)
+    print(f"{angle_min},{angle_max},{angle_increment}")
+
+    #Actualizar el grafo de obstaculos
+
+    """ if (min(ranges) <= scale):
         obstacles = [(round(currentx + ranges[i] * 100 * math.cos(i * (math.pi / 180) + yaw)), round(currenty +
                                                                                                      ranges[
                                                                                                          i] * 100 * math.sin(
@@ -53,7 +62,7 @@ def callback_laser(data):
             obst_neighbours.append((obst_neighbour))
         obst_neighbours = np.array(obst_neighbours)
         obst_neighbours = np.unique(obst_neighbours, axis=0)
-        print(obst_neighbours)
+        print(obst_neighbours) """
 
 def callback_angle(data):
     global yaw
@@ -66,61 +75,22 @@ def callback_angle(data):
     roll, pitch, yaw = euler_from_quaternion(
         [rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
-def is_on_obstacle(x,y,clr = 0):
-    o1, o2, o3, o4, o5, \
-    o6, o7, o8, o9, o10, \
-    o11, o12, o13, o14, o15, \
-    o16, o17 = False, False, False, False, \
-               False, False, False, False, \
-               False, False, False, False, \
-               False, False, False, False, False
-    if ((x + 405.05)**2 + (y - 325.05)**2 - (79.95+clr)**2) <= 0 \
-            or ((x + 245.27)**2 + (y - 325.05)**2 - (79.95+clr)**2) <= 0 \
-            or (y - 245.1 + clr >= 0 and y - 405 - clr <= 0 and x + 245.27 - clr <= 0 and x + 405.05 + clr >= 0):
-        o1 = True
-    if (x+165)**2 + (y-460)**2 - (40.5 + clr)**2 <=0:
-        o2 = True
-    if (x+117)**2 + (y-231)**2 - (40.5 + clr)**2 <=0:
-        o3 = True
-    if (y + 190 + clr >= 0 and y + 7 - clr <= 0 and x + 26 - clr <= 0 and x + 117 + clr>= 0):
-        o4 = True
-    if (x+117)**2 + (y+231)**2 - (40.5 + clr)**2 <=0:
-        o5 = True
-    if (x+165)**2 + (y+460)**2 - (40.5 + clr)**2 <=0:
-        o6 = True
-    if (y + 240 + clr >= 0 and y + 164 - clr <= 0 and x + 26 + clr >= 0 and x - 157 - clr <= 0):
-        o7 = True
-    if (x + 81 + clr >= 0 and x - 193 - clr <=0 and y + 470 + clr >=0 and y + 318 - clr <=0):
-        o8 = True
-    if (x - 224 + clr >= 0 and x - 341 - clr <=0 and y + 470 + clr >=0 and y + 412 - clr <=0):
-        o9 = True
-    if (x - 229.5 + clr >= 0 and x - 381.5 - clr <=0 and y + 121 - clr <=0 and y + 238 + clr >=0):
-        o10 = True
-    if (x - 372 + clr >= 0 and x - 555 - clr <=0 and y + 470 + clr >=0 and y + 394 - clr <=0):
-        o11 = True
-    if (x - 497 + clr >= 0 and x - 555 - clr <=0 and y + 209.75 - clr <=0 and y + 326.75 + clr >=0):
-        o12 = True
-    if (x - 464 + clr >= 0 and x - 555 - clr <=0 and y + 142.5 + clr >=0 and y + 56.5 - clr <=0):
-        o13 = True
-    if (x - 497 + clr >= 0 and x - 555 - clr <=0 and y + 56.5 + clr >=0 and y - 60.5 - clr <=0):
-        o14 = True
-    if (x - 189 + clr >= 0 and x - 555 - clr <=0 and y - 116 + clr >=0 and y - 192 - clr <=0):
-        o15 = True
-    if (x - 277 + clr >= 0 and x - 363 - clr <=0 and y - 322 + clr >=0 and y - 505 - clr <=0):
-        o16 = True
-    if (x - 471 - clr <= 0 and x - 428 + clr >=0 and y - 414 + clr >=0 and y - 505 - clr <=0):
-        o17 = True
-    return o1 or o2 or o3 or o4 or o5 or o6 or o7 or o8 or o9 or o10 or o11 or o12 or o13 or o14 or o15 or o16 or o17
-
 def newOdom(msg):
     global x
     global y
     global theta
+    global x_pixel_r
+    global y_pixel_r
 
     x = msg.pose.pose.position.x
     y = msg.pose.pose.position.y
+    x_pixel_r = int((x)*555/10)
+    y_pixel_r = int((x)*505/10)
+
     quat = msg.pose.pose.orientation
     (_, _, theta) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+    
+
 
 ########################################################################################################################
 # Class structures
@@ -134,6 +104,8 @@ class Node:
         self.rhs = 999999  # should be infinite
 
 class Graph:
+    subDepth=None
+    bg = None
     # graph constructor that creates an empty dictionary
     # nodes = {(x,y):Node} where x,y are coordinates of node
     # open_list = {(x,y): key}
@@ -143,6 +115,48 @@ class Graph:
         self.obstacle_space = set()
         self.current_path = []
         self.open_length = 0
+
+    def callback_laser(self,data):
+        global yaw
+        global currentx
+        global currenty
+        global obstacles
+        global scale
+        global neighbours
+        global obst_neighbours
+
+        obst_neighbours = []
+        # range_min = data.range_min
+        angle_min = data.angle_min
+        angle_max = data.angle_max
+        angle_increment = data.angle_increment
+        ranges = np.array(data.ranges)
+        #print(ranges)
+        #print(f"{angle_min},{angle_max},{angle_increment}")
+        angulo_actual = 0
+        x_relativa = 0
+        y_relativa = 0
+        count = 0
+        for i in ranges:
+            if i != float("inf"):
+                angulo_actual = count*angle_increment
+                x_relativa = math.cos((angulo_actual+theta))*i #Sacando la posición relativa de los puntos respecto al eje principal del mapa
+                y_relativa = math.sin((angulo_actual+theta))*i
+                #Ahora hace falta meter esto en la lista negra exceptuando que sea un objetivo por el mapa indicado
+                #Por ahora vamos a asumir que todo es obstaculo
+                x_pixel = int((x + x_relativa)*555/10)
+                y_pixel = int((y + y_relativa)*505/10)
+                self.obstacle_space.add((x_pixel,y_pixel)) #Lo añadimos a obstaculosç
+                Graph.bg[505-y_pixel,555+x_pixel] = (0,0,0)
+            #Incrementamos el objeto
+            count = count + 1
+        
+
+
+
+        
+
+
     # loop through image and create node object for each pixel
     def create_nodes(self):
         for x in range(-555, 555):
@@ -181,6 +195,7 @@ class Graph:
         if (x+1,y) not in self.obstacle_space and x+1 < 555:
             if (x+1,y) not in self.open_list and self.nodes[(x+1,y)].rhs == 999999:
                 self.nodes[(x,y)].neighbours[(x+1,y)] = strght
+    
     # Check if node is consistent
     def node_is_consistent(self,node):
         if self.nodes[node].g_cost == self.nodes[node].rhs:
@@ -197,7 +212,7 @@ class Graph:
         return key
     # Get smallest element from the open_list:
     def get_smallest(self,open_list):
-        smallest = 9999999;
+        smallest = 9999999
         smallest_node = (-9999,-9999)
         for key, value in open_list.items():
             if open_list[key] < smallest:
@@ -325,21 +340,27 @@ class Graph:
             curr_node = self.get_smallest(new_open_list)
         new_path = []
         count = 0
+        print(f"Nodo actual: {curr_node}")
         while not self.nodes[curr_node].parent == None:
             #while not curr_node == goal_node:
-            bg[505 - curr_node[1], curr_node[0] + 555] = (250, 0, 0)
+            Graph.bg[505 - curr_node[1], curr_node[0] + 555] = (250, 0, 0)
             new_path.append(curr_node)
             curr_node = self.nodes[curr_node].parent
             count += 1
         new_path.append(curr_node)
         return new_path
+
     # Travel across the received path
     def traverse(self,bg,current_path,rob_x,rob_y):
+        global x_pixel_r
+        global y_pixel_r
         #self.obstacle_space.add((18, 0))
         #bg[505 - 18, 0 + 555] = (0, 0, 0)
+        Graph.bg = bg
         rospy.init_node('speed_controller')
         sub = rospy.Subscriber('/odom', Odometry, newOdom)
         pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        subDept = rospy.Subscriber("/scan",LaserScan,self.callback_laser)
         rate = rospy.Rate(4)
         new_path = []
         for i in current_path:
@@ -358,14 +379,9 @@ class Graph:
             for g in new_path:
                 p = current_path[i]
                 i += 1
-                bg[505 - p[1], p[0] + 555] = (255, 255, 255)
+                Graph.bg[505 - p[1], p[0] + 555] = (255, 255, 255)
                 print("P:",p)
-                if p[0] == 78:
-                   for l in range(80, 93):
-                       for m in range(-35, 35):
-                           self.obstacle_space.add((l, m))
-                           bg[505 - m, l + 555] = (0, 0, 0)
-                           rob_x = 128
+                #Esta función hace llamada recursiva para recalcular ruta
                 if self.nodes[p].parent in self.obstacle_space:
                     print("Obstacle found at ", self.nodes[p].parent)
                     parent = self.nodes[p].parent
@@ -373,10 +389,9 @@ class Graph:
                     while parent in self.obstacle_space:
                         print("Obstacle found at ", parent)
                         parent = self.nodes[parent].parent
-                    new_path = self.replan(p[0], p[1], rob_x, rob_y, current_path, bg)
-                    self.traverse(bg, new_path,rob_x, rob_y)
-                    break
-                cv2.imshow("Output", bg)
+                    new_path = self.replan(p[0], p[1], rob_x, rob_y, current_path, Graph.bg)
+                    self.traverse(Graph.bg, new_path,150, 0)
+                cv2.imshow("Output", Graph.bg)
                 cv2.waitKey(1)
                 del_x = g.x - x
                 del_y = g.y - y
@@ -420,26 +435,25 @@ def initiate():
     bg = np.zeros((height,width,3),dtype=np.uint8)
     # Calculate Mikowski space
     r = 25
-    print("Calculating Minowski Space. please wait!!")
     graph = Graph()
+    """
+        Define obstaculos manualmente
+    """
     # iterate for each pixel and find out if it is an obstacle
     # if it is in the obstacle store it in the obstacle set
-    for x in range(-555, 555): #Relacionado con la resolución
-        for y in range(-505, 505): #Relacionado con la resolución
-            y = -y
-            if is_on_obstacle(x, y, r):
-                graph.obstacle_space.add((x, y))
+    #for x in range(-555, 555): #Relacionado con la resolución
+    #    for y in range(-505, 505): #Relacionado con la resolución
+    #        y = -y
+    #        if is_on_obstacle(x, y, r):
+    #            graph.obstacle_space.add((x, y))
+
+
     graph.create_nodes()
     for node in graph.nodes:
         x = node[0]
         y = node[1]
         # y = -y
         bg[505 - y, x + 555] = (192, 192, 192) #color gris para nodos no visitados
-    #bg2 = cv2.resize(bg, (550, 505))
-    #cv2.imshow("colored.png", bg)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
     x_r = 0 #Antes era -300, siendo el inicio de robot malo
     y_r = 0
     x_g = 200
@@ -450,10 +464,6 @@ def initiate():
     y = y_r
     print("Goal",x_g,y_g)
     current_path = graph.d_star_lite_algo(x_r,y_r,x_g,y_g,bg)
-    #for i in range(80, 132):
-    #    for j in range(-35, 35):
-    #        graph.obstacle_space.add((i, j))
-    #        bg[505 - j, i + 555] = (0, 0, 0) #Le pone 
     graph.traverse(bg,current_path,150,0)
 if __name__ == '__main__':
 	try:
